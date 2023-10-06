21410ce6bf71b6918437de41c08b1deebf7a11f6
Kaila <kailaamia@gmail.com>
2023-10-06 00:12:05 -0500

    Added questions and answers

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,4 +1,39 @@
 export const testQuestions = {
+    q1: {
+        question:'What is DOM (Document Object Model)?',
+        correct:'It is a model of the HTML elements constructed as a tree.',
+        incorrect1:'It is a model of the CSS elements constructed as a tree.',
+        incorrect2:'It is a function associated with an object.',
+        incorrect3:'Bro I actually have no idea.',
+    },
+    q2: {
+        question:'What is a function is js?',
+        correct:'It is a reusable block of code.',
+        incorrect1:'It is an attribute of an object.',
+        incorrect2:'It is a property that allows you to store data in the browser local storage.',
+        incorrect3:'It is a concept in javascript that refers to the way that events propagate up.',
+    },
+    q3: {
+        question:'What is a property?',
+        correct:'It is an attribute of an object.',
+        incorrect1:'It is a concept in javascript that refers to the way that events propagate up.',
+        incorrect2:'It is a read only value of the window interface.',
+        incorrect3:'It is a method of an object.',
+    },
+    q4: {
+        question:'Which method of the localStorage property retrieves the value of a key?',
+        correct:'getItem()',
+        incorrect1:'setItem()',
+        incorrect2:'removeItem()',
+        incorrect3:'findItem()',
+    },
+    q5: {
+        question:'What is the correct syntax of an event listener?',
+        correct:'.addEventListener(type, listener)',
+        incorrect1:'eventListener(listerner, options)',
+        incorrect2:'addEventListener(listener, options)',
+        incorrect3:'addEventListener(type, options)',
+    },
     
 };
 
