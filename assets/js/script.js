527f47eff03566abc8b035fe1b14ad538000c730
Kaila <kailaamia@gmail.com>
2023-10-05 22:22:58 -0500

    Added all element variables in js file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,21 @@
 import {
     testQuestions,
 } from './questions.js';
+
+//Screens
+let homeScreen = document.querySelector('main');
+let gameScreen = document.querySelector('#game-view')
+
+//buttons
+let startBtn = document.querySelector('#start-btn');
+let scoreBtn = document.querySelector('#score-btn');
+
+//Question
+let questionNum = document.querySelector('#question-num')
+let questionText = document.querySelector('#question-text');
+
+//Answers
+let answer1 = document.querySelector('#answer-1');
+let answer2 = document.querySelector('#answer-2');
+let answer3 = document.querySelector('#answer-3');
+let answer4 = document.querySelector('#answer-4');
